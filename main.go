22f26052b7b39b512d9db2edea0cb74ac754982d
sinmetal <metal.tie@gmail.com>
2018-09-28 19:06:46 +0900

    ログ出力するサンプル値を追加

--- main.go
+++ main.go
@@ -6,6 +6,28 @@ import (
 
 func main() {
 	for {
+		fmt.Printf("%d\n", uint64(36028797018963970))
+		fmt.Printf("%d\n", uint64(36028797018963969))
 		fmt.Printf("%d\n", uint64(36028797018963968))
+		fmt.Printf("%d\n", uint64(36028797018963967))
+		fmt.Printf("%d\n", uint64(36028797018963966))
+		fmt.Printf("%d\n", uint64(36028797018963965))
+		fmt.Printf("%d\n", uint64(36028797018963964))
+		fmt.Printf("%d\n", uint64(36028797018963963))
+		fmt.Printf("%d\n", uint64(36028797018963962))
+		fmt.Printf("%d\n", uint64(36028797018963961))
+		fmt.Printf("%d\n", uint64(36028797018963960))
+		fmt.Printf("%d\n", uint64(36028797018963959))
+		fmt.Printf("%d\n", uint64(36028797018963958))
+		fmt.Printf("%d\n", uint64(36028797018963957))
+		fmt.Printf("%d\n", uint64(36028797018963956))
+		fmt.Printf("%d\n", uint64(36028797018963955))
+		fmt.Printf("%d\n", uint64(36028797018963954))
+		fmt.Printf("%d\n", uint64(36028797018963953))
+		fmt.Printf("%d\n", uint64(36028797018963952))
+		fmt.Printf("%d\n", uint64(36028797018963951))
+		fmt.Printf("%d\n", uint64(36028797018963950))
+		fmt.Printf("%d\n", uint64(36028797018963859))
+		fmt.Printf("%d\n", uint64(26028797018963969))
 	}
 }
