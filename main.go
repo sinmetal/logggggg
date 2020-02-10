a8e25bf20929c2a739f90f532e006c2b9c49441e
sinmetal <metal.tie@gmail.com>
2020-02-10 21:38:18 +0900

    起動時に一発動かすようにした

--- main.go
+++ main.go
@@ -9,18 +9,24 @@ func main() {
 	t := time.NewTicker(6 * time.Minute)
 	defer t.Stop()
 
+	run(context.Background()) // Ticker待つのがめんどいので、とりあえず一発出す
+
 	for {
 		select {
 		case <-t.C:
 			ctx := context.Background()
-			outputBigNumber()
-
-			msg := &MessageV1{
-				LogKindName: "Sample",
-				Version:     "v1.0.0",
-				Body:        "Hello World",
-			}
-			LogMessageV1(ctx, msg)
+			run(ctx)
 		}
 	}
+}
+
+func run(ctx context.Context) {
+	outputBigNumber()
+
+	msg := &MessageV1{
+		LogKindName: "Sample",
+		Version:     "v1.0.0",
+		Body:        "Hello World",
+	}
+	LogMessageV1(ctx, msg)
 }
